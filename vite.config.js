02d6c94898cf430b135c7856cf933665db935280
Mike Zeng <mzeng1417@gmail.com>
2023-09-20 14:43:12 -0500

    Fix base-url

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "threejs-donut",
+  base: "ThreeJS-Donut",
   server: {
     host: true,
     port: 3000,
